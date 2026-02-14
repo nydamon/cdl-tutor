9264fa52b0296cbf66e0586a3f281cb6c311a5d4
Damon <damon@thecreditpros.com>
2026-02-14 19:00:29 +0000

    Working version with state data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { readFileSync, existsSync } = require('fs');
 const { join } = require('path');
 
 const PORT = process.env.PORT || 3000;
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 const mimeTypes = {
   '.html': 'text/html',
@@ -16,84 +17,68 @@ const mimeTypes = {
   '.webmanifest': 'application/manifest+json'
 };
 
-// Simple router
-const routes = {
-  '/health': { status: 200, json: { status: 'ok', version: '1.0.0' }},
-  '/api/states': { 
-    status: 200, 
-    json: [
-      { code: 'AL', name: 'Alabama' }, { code: 'AK', name: 'Alaska' },
-      { code: 'AZ', name: 'Arizona' }, { code: 'AR', name: 'Arkansas' },
-      { code: 'CA', name: 'California' }, { code: 'CO', name: 'Colorado' },
-      { code: 'CT', name: 'Connecticut' }, { code: 'DE', name: 'Delaware' },
-      { code: 'FL', name: 'Florida' }, { code: 'GA', name: 'Georgia' },
-      { code: 'HI', name: 'Hawaii' }, { code: 'ID', name: 'Idaho' },
-      { code: 'IL', name: 'Illinois' }, { code: 'IN', name: 'Indiana' },
-      { code: 'IA', name: 'Iowa' }, { code: 'KS', name: 'Kansas' },
-      { code: 'KY', name: 'Kentucky' }, { code: 'LA', name: 'Louisiana' },
-      { code: 'ME', name: 'Maine' }, { code: 'MD', name: 'Maryland' },
-      { code: 'MA', name: 'Massachusetts' }, { code: 'MI', name: 'Michigan' },
-      { code: 'MN', name: 'Minnesota' }, { code: 'MS', name: 'Mississippi' },
-      { code: 'MO', name: 'Missouri' }, { code: 'MT', name: 'Montana' },
-      { code: 'NE', name: 'Nebraska' }, { code: 'NV', name: 'Nevada' },
-      { code: 'NH', name: 'New Hampshire' }, { code: 'NJ', name: 'New Jersey' },
-      { code: 'NM', name: 'New Mexico' }, { code: 'NY', name: 'New York' },
-      { code: 'NC', name: 'North Carolina' }, { code: 'ND', name: 'North Dakota' },
-      { code: 'OH', name: 'Ohio' }, { code: 'OK', name: 'Oklahoma' },
-      { code: 'OR', name: 'Oregon' }, { code: 'PA', name: 'Pennsylvania' },
-      { code: 'RI', name: 'Rhode Island' }, { code: 'SC', name: 'South Carolina' },
-      { code: 'SD', name: 'South Dakota' }, { code: 'TN', name: 'Tennessee' },
-      { code: 'TX', name: 'Texas' }, { code: 'UT', name: 'Utah' },
-      { code: 'VT', name: 'Vermont' }, { code: 'VA', name: 'Virginia' },
-      { code: 'WA', name: 'Washington' }, { code: 'WV', name: 'West Virginia' },
-      { code: 'WI', name: 'Wisconsin' }, { code: 'WY', name: 'Wyoming' }
-    ]
-  },
-  '/api/state/NJ': {
-    status: 200,
-    json: {
-      state: 'NJ',
-      state_name: 'New Jersey',
-      topics: [
-        { name: 'General Knowledge', must_memorize: ['4-second following distance', '5-6 seconds for trucks'] },
-        { name: 'Air Brakes', must_memorize: ['60 psi warning', '20-45 psi spring brakes'] },
-        { name: 'Hazardous Materials', must_memorize: ['0.04% BAC'] }
-      ]
-    }
-  }
+// State data
+const stateData = {
+  NJ: {state:'NJ',state_name:'New Jersey',topics:[{name:'General Knowledge',must_memorize:['4-second following distance','5-6 seconds for trucks']},{name:'Air Brakes',must_memorize:['60 psi warning','20-45 psi spring brakes']},{name:'Hazardous Materials',must_memorize:['0.04% BAC']}]},
+  TX: {state:'TX',state_name:'Texas',topics:[{name:'General Knowledge',must_memorize:['4-second following distance','55 mph max']},{name:'Air Brakes',must_memorize:['60 psi warning']},{name:'Hazardous Materials',must_memorize:['0.04% BAC']}]},
+  CA: {state:'CA',state_name:'California',topics:[{name:'General Knowledge',must_memorize:['3-second rule']},{name:'Air Brakes',must_memorize:['60 psi warning']},{name:'Hazardous Materials',must_memorize:['0.04% BAC']}]},
+  FL: {state:'FL',state_name:'Florida',topics:[{name:'General Knowledge',must_memorize:['4-second following distance']},{name:'Air Brakes',must_memorize:['60 psi warning']},{name:'Hazardous Materials',must_memorize:['0.04% BAC']}]},
+  NY: {state:'NY',state_name:'New York',topics:[{name:'General Knowledge',must_memorize:['4-second following distance']},{name:'Air Brakes',must_memorize:['60 psi warning']},{name:'Hazardous Materials',must_memorize:['0.04% BAC','TWIC']}]}
 };
 
-const server = createServer((req, res) => {
-  const url = parse(req.url, true);
-  const pathname = url.pathname;
+const statesList = [
+  {code:'AL',name:'Alabama'},{code:'AK',name:'Alaska'},{code:'AZ',name:'Arizona'},{code:'AR',name:'Arkansas'},
+  {code:'CA',name:'California'},{code:'CO',name:'Colorado'},{code:'CT',name:'Connecticut'},{code:'DE',name:'Delaware'},
+  {code:'FL',name:'Florida'},{code:'GA',name:'Georgia'},{code:'HI',name:'Hawaii'},{code:'ID',name:'Idaho'},
+  {code:'IL',name:'Illinois'},{code:'IN',name:'Indiana'},{code:'IA',name:'Iowa'},{code:'KS',name:'Kansas'},
+  {code:'KY',name:'Kentucky'},{code:'LA',name:'Louisiana'},{code:'ME',name:'Maine'},{code:'MD',name:'Maryland'},
+  {code:'MA',name:'Massachusetts'},{code:'MI',name:'Michigan'},{code:'MN',name:'Minnesota'},{code:'MS',name:'Mississippi'},
+  {code:'MO',name:'Missouri'},{code:'MT',name:'Montana'},{code:'NE',name:'Nebraska'},{code:'NV',name:'Nevada'},
+  {code:'NH',name:'New Hampshire'},{code:'NJ',name:'New Jersey'},{code:'NM',name:'New Mexico'},{code:'NY',name:'New York'},
+  {code:'NC',name:'North Carolina'},{code:'ND',name:'North Dakota'},{code:'OH',name:'Ohio'},{code:'OK',name:'Oklahoma'},
+  {code:'OR',name:'Oregon'},{code:'PA',name:'Pennsylvania'},{code:'RI',name:'Rhode Island'},{code:'SC',name:'South Carolina'},
+  {code:'SD',name:'South Dakota'},{code:'TN',name:'Tennessee'},{code:'TX',name:'Texas'},{code:'UT',name:'Utah'},
+  {code:'VT',name:'Vermont'},{code:'VA',name:'Virginia'},{code:'WA',name:'Washington'},{code:'WV',name:'West Virginia'},
+  {code:'WI',name:'Wisconsin'},{code:'WY',name:'Wyoming'}
+];
+
+const routes = {
+  '/health': {json:{status:'ok',version:'1.0.1'}},
+  '/api/states': {json:statesList}
+};
+
+const server = createServer(async (req, res) => {
+  const { pathname } = parse(req.url);
   
-  // API routes
+  // State API
+  if (pathname.startsWith('/api/state/')) {
+    const code = pathname.split('/').pop().toUpperCase();
+    const data = stateData[code] || {state:code,state_name:code,topics:[],message:'Coming soon'};
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify(data));
+    return;
+  }
+  
+  // Static routes
   if (routes[pathname]) {
-    const route = routes[pathname];
-    res.writeHead(route.status, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(route.json));
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify(routes[pathname].json));
     return;
   }
   
   // Serve static files
   let filePath = join(__dirname, 'public', pathname === '/' ? 'index.html' : pathname);
-  
-  if (!existsSync(filePath)) {
-    filePath = join(__dirname, 'public', 'index.html');
-  }
+  if (!existsSync(filePath)) filePath = join(__dirname, 'public', 'index.html');
   
   if (existsSync(filePath)) {
     const ext = filePath.match(/\.[^.]+$/)?.[0] || '.html';
-    const contentType = mimeTypes[ext] || 'text/plain';
-    const content = readFileSync(filePath);
-    res.writeHead(200, { 'Content-Type': contentType });
-    res.end(content);
+    res.writeHead(200, {'Content-Type':mimeTypes[ext]||'text/plain'});
+    res.end(readFileSync(filePath));
   } else {
     res.writeHead(404);
     res.end('Not Found');
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`CDL Tutor running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`CDL Tutor on ${PORT}`));
+module.exports = server;
